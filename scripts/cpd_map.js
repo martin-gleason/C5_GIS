128cf8e24f716f429bccfc0756efd4d5040e7be5
marty-gleason <officergleason@gmail.com>
2018-09-18 15:40:59 -0500

    Remove requirement to initMap() in html. CPD is now own function. working on updating map with geocode submit

--- scripts/cpd_map.js
+++ scripts/cpd_map.js
@@ -1,15 +1,19 @@
-//Map and geometry creation
+//Map and Police district
 
-var map;
-var data;
+$(document).ready(function(){
 
-function initMap(){
-	map = new google.maps.Map(document.getElementById("map"), {
-		center: {lat: 41.8676354, lng:-87.68105109999999},
-		zoom: 11
-	});
-var data = map.data.loadGeoJson("http://127.0.0.1:9090/CPD-districts.geojson");
-map.data.addGeoJson(data);
+	var map;
+	var data;
 
+	function initMap(){
+		map = new google.maps.Map(document.getElementById("map"), {
+			center: {lat: 41.8676354, lng:-87.68105109999999},
+			zoom: 12
+		});
+	
+	var data = map.data.loadGeoJson("http://127.0.0.1:9090/CPD-districts.geojson");
+	map.data.addGeoJson(data);
 
-};
+	};
+	google.maps.event.addDomListener(window, 'load', initMap)
+});
--- scripts/geocoding_google.js
+++ scripts/geocoding_google.js
@@ -1,29 +1,32 @@
 //API key and geocoding Work
 
-var key = "AIzaSyCdNaEzVGrwgTU1dW9N34dNLdF2uTeyrmE";
-
-var url_base = "https://maps.googleapis.com/maps/api/geocode/outputFormat?";
-
 var geocoder = new google.maps.Geocoder();
+var latLongUpdate;
 
 
 function codeAddress(x){
 	$("div.test").prepend(" ");
 	space = new RegExp(/\s/g);	
 	address = x.replace(space, "+");
-	console.log(address);
+
 	
 	geocoder.geocode( {"address" : address}, function(results, status) {
 		if (status == "OK") {
-			$("div.test").append(results[0].geometry.location.toString());			
+			latLongUpdate = results[0].geometry.location;
+			alert(latLongUpdate.toString());
+			$("div.test").append(latLongUpdate.toString());			
 		} else {
 				alert("The geocode status was: " + status)};
-			
-			});
+	});
+
+// 		var marker = new google.maps.Marker({
+// 		
+// 			position: latLongUpdate,
+// 			map: map,
+// 			title: "Client Address"
+// 		
+// 		});
+
 };
 
 
-function load_map(){
-	
-
-};
